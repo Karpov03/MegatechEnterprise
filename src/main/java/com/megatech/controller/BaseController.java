53a5d6b2e568ac0200ae014951f53f8efdc57366
Karpov <Karpov03@gmail.com>
2017-04-08 17:41:28 +0530

    EnergyMonitor Version 1.0

--- src/main/java/com/megatech/controller/BaseController.java
+++ src/main/java/com/megatech/controller/BaseController.java
@@ -2,18 +2,12 @@ package com.megatech.controller;
 
 
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.megatech.logics.MainLogics;
-
 @RestController
 public class BaseController {
-	@Autowired
-	MainLogics mainLogic;
+	
 
 	@GetMapping(value = { "/", "" })
 	public String hellopage() {
@@ -21,9 +15,4 @@ public class BaseController {
 		return "WELCOME TO ENTERPRISE WEBAPP";
 	}
 
-	@GetMapping(value = { "/avg" })
-	public List<?> average() {
-		System.out.println("Computing Average");
-		 return mainLogic.findAll();
-	}
 }
